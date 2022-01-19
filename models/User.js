224144d4519ea78143ed605bbee6c6457d7a5ce3
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2022-01-19 09:38:56 -0800

    Updated default structure and added first model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  vigama: {
+    level: Number,
+    vigamaPoints: Number,
+    skillPoints: Number,
+    unrankedVigamaPoints: Number,
+    habitPoints: Number,
+  },
+});
+
+module.exports = mongoose.model("user", UserSchema);
