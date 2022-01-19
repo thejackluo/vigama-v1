b963a52a78eec714b02ff8eca28c67d4089ebfa0
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2022-01-19 09:24:33 -0800

    Removed regular React Template

---
+++ src/Pages/Template/Template.js
@@ -0,0 +1,21 @@
+// * React Imports
+
+import React, { useState } from "react";
+
+// * External Library Imports
+
+// * General Application Imports
+
+// * Page Specific Imports
+
+// Import CSS File here
+
+// * Function
+
+const Template = () => {
+  return <h1>Hello World</h1>;
+};
+
+// * Export
+
+export default Template;
