a7d631650ed0daf36b0663e6d7d04ccdde2f3191
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2022-02-09 20:27:42 -0800

    Improve on templates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Application Testing</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ static/vigama.js
@@ -0,0 +1,19 @@
+let level = 1;
+let totalXP = 0;
+let levelSpecificXP = 0;
+let vp = 0;
+
+// Map with key value pairs
+const specializedPoints = new Map();
+
+const levelUp = () => {
+    level++;
+}
+
+const studyHours 
+
+const main() = () => {
+
+}
+
+main();
