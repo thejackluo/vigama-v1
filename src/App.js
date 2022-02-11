090f64712b1b3956b97984446de625fec50afcb8
Jack Luo <30420522+thejackluo@users.noreply.github.com>
2022-02-11 14:24:42 -0800

    App.js added test components for designing mech

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React, { useState } from "react";
+
 import "./App.css";
 
 function App() {
+  const [level, useLevel] = useState(1);
+  const [totalXP, useTotalXP] = useState(0);
+  const [levelSpecificXP, useLeveSpecificXP] = useState(0);
+  const [vp, useVP] = useState(0);
+
+  const handleClickButton1 = () => {};
+
   return (
     <div>
-      <h1>Application Testing</h1>
+      <h1>Data testing for software mechanism</h1>
+      <ul>
+        <li>Level: {level}</li>
+        <li>Total XP: {totalXP}</li>
+        <li>Level Specific XP: {levelSpecificXP}</li>
+        <li>Vigama Points: {vp}</li>
+      </ul>
+      <div>
+        <button onClick={handleClickButton1}>Button 1</button>
+        <button>Button 2</button>
+        <button>Button 3</button>
+        <button>Button 4</button>
+      </div>
     </div>
   );
 }
