fdc2c8d2abfc08cb0c791603c1a37b067088e7ec
Jack Luo <jack145945@gmail.com>
2023-04-13 14:26:29 -0700

    Updated template.tsx file to match specifications

--- components/template.tsx
+++ components/template.tsx
@@ -1,23 +1,40 @@
 /*
+  Name: template.tsx
   Author: Jack Luo
-  Description: This is the template file
+  Description: This is a template file that contains a complete documentation on the specifications of the specific component (including imports, 
+  api routes, state changes, etc)
+
+  === Imports === 
+  Next.js, React.js, Chakra, React Hooks
+
+  === Component Imports ===
+  
+  
+  === State changes ===
+
+
+  === API routes ===
+
+
+  === Component Structure ===
+
+
 */
 
-// Next Imports
-import * as N from "next";
-
-// Chakra imports
-import * as J from "@chakra-ui/react";
-
-// Style Inputs
-import styles from "@/styles/Home.module.css";
-
-// React Imports
-import { useState } from "react";
+// Default Imports (required for each file)
+import * as N from "next"; // Next.js
+import { useState, useEffect } from "react"; // React Hooks
+import * as J from "@chakra-ui/react"; // Chakra UI
+import "@/styles/globals.css"; // Global CSS
 
 // Component Imports
 import Profile from "@/components/reusable/profile";
 
+// State changes
+
+// API routes
+
+// Component Structure
 export default function Home() {
   return (
     <>
