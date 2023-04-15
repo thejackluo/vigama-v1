6788000f5d4390b47b554f14fc70c0a9f9487b4f
Jack Luo <jack145945@gmail.com>
2023-04-14 17:53:53 -0700

    Removed profile.json

--- backend/models/gamification/GameModel.js
+++ backend/models/gamification/GameModel.js
@@ -3,19 +3,34 @@ const Schema = mongoose.Schema;
 
 const GameSchema = new Schema({
   // general game info
-    general: {
-        xp: {
-            type: Number,
-            default: 0
-        },
-        level: {
-            type: Number,
-            default: 0
-        },
-        coins: {
-            type: Number,
-            default: 0
-        },
+  general: {
+    xp: {
+      type: Number,
+      default: 0,
+    },
+    level: {
+      type: Number,
+      default: 0,
+    },
+    coins: {
+      type: Number,
+      default: 0,
+    },
+    timecoins: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  // game productivity stats
+  productivity: {
+    // to be continued
+  },
+
+  // cilivization stats
+  polytopia: {
+    // to be continued
+  },
 });
 
 module.exports = mongoose.model("Game", GameSchema);
